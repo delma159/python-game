1b57e5a8a2ed7f75e03bdc57c22df988e243771c
DEVASHISH7777 <63115818+DEVASHISH7777@users.noreply.github.com>
2020-12-13 17:48:17 +0100

    Update items.py

--- backend/python/items.py
+++ backend/python/items.py
@@ -1,12 +1,16 @@
+from map import MapPrincess, Controleur
+from case_type import CaseType
 
+
+Map = MapPrincess(5, 5)
 
 class Items:
 
-    def __init__(self,bonus,malus):
-
-        self.bonus = bonus
-        self.malus = malus
-
+    def __init__(self):
+        self.bonus = 0
+        self.malus = 0
+        self.type = {1: ' + ', 2: ' - '}
+        self.point = 0
 
     @property
     def get_bonus(self):
@@ -15,3 +19,13 @@ class Items:
     @property
     def get_malus(self):
         return self.malus
+
+    def generate_items(self):
+        liste = Map.listeMap
+        case = CaseType()
+        Map.generate_map()
+
+        nrb = liste[-3].index(case.contenue_case[1])
+        liste[-3][nrb] = self.type[1]
+
+
