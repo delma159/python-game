79a353a6b253808250efa5cde6ad21fbbf53b0db
DEVASHISH7777 <63115818+DEVASHISH7777@users.noreply.github.com>
2020-12-13 17:52:44 +0100

    Update interface.py

--- backend/python/interface.py
+++ backend/python/interface.py
@@ -23,19 +23,19 @@ class MyApp:
     def create_widgets(self):
         self.create_title()
         self.create_subtitle()
+        self.create_button()
 
     def create_title(self):
-        label_title = Label(self.frame, text="Bienvenue sur Princess Rescue", font=("Courrier", 40), bg='#41B77F',
-                            fg='white')
+        label_title = Label(self.frame, text="Bienvenue sur Princess Rescue", font=("Courrier", 40), bg='#41B77F',fg='white')
         label_title.pack()
 
     def create_subtitle(self):
-        label_subtitle = Label(self.frame, text="Amusez-vous", font=("Courrier", 25), bg='#41B77F',
-                               fg='white')
+        label_subtitle = Label(self.frame, text="Amusez-vous", font=("Courrier", 25), bg='#41B77F', fg='white')
         label_subtitle.pack()
 
     def create_button(self):
-        button = Button(text="start", font=('Courrier', 25), bg='White')
+        label_button = Button(self.frame, text="start", font=('Courrier', 25), bg='White')
+        label_button.pack()
 
 
 
