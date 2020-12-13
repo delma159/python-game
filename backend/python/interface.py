951c57326e9c34e19eec2f4ea5614d3559308143
DEVASHISH7777 <63115818+DEVASHISH7777@users.noreply.github.com>
2020-12-13 17:47:49 +0100

    Add files via upload

---
+++ backend/python/interface.py
@@ -0,0 +1,46 @@
+from tkinter import *
+
+
+class MyApp:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Princess Rescue")
+        self.window.geometry("1080x720")
+        self.window.minsize(480, 360)
+        self.window.iconbitmap("")
+        self.window.config(background='#41B77F')
+
+        # initialization des composants
+        self.frame = Frame(self.window, bg='#41B77F')
+
+        # creation des composants
+        self.create_widgets()
+
+        # empaquetage
+        self.frame.pack(expand=YES)
+
+    def create_widgets(self):
+        self.create_title()
+        self.create_subtitle()
+
+    def create_title(self):
+        label_title = Label(self.frame, text="Bienvenue sur Princess Rescue", font=("Courrier", 40), bg='#41B77F',
+                            fg='white')
+        label_title.pack()
+
+    def create_subtitle(self):
+        label_subtitle = Label(self.frame, text="Amusez-vous", font=("Courrier", 25), bg='#41B77F',
+                               fg='white')
+        label_subtitle.pack()
+
+    def create_button(self):
+        button = Button(text="start", font=('Courrier', 25), bg='White')
+
+
+
+
+
+# afficher
+app = MyApp()
+app.window.mainloop()
