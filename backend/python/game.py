d4a5432a7c17719ffc8e2bb6db5b229bd48eece0
DEVASHISH7777 <63115818+DEVASHISH7777@users.noreply.github.com>
2020-12-31 18:30:23 +0100

    ajout obstacle
    spawn un obstacle
     gestion collision

--- backend/python/game.py
+++ backend/python/game.py
@@ -5,3 +5,14 @@ class Game:
 
     def __init__(self):
         self.player = Player()
+        self.all_obstacle = pygame.sprite.Group()
+        self.pressed = {}
+        self.spawn_obstacle()
+
+    def spawn_obstacle(self):
+        obstacle = Obstacle()
+        self.all_obstacle.add(obstacle)
+
+    def check_collision(self, sprite, group):
+
+        return pygame.sprite.spritecollide(sprite, group, False, pygame.sprite.collide_mask)
