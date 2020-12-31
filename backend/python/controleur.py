b89cdf76b0f3b2d311be2dbe643ee10708efbe3f
DEVASHISH7777 <63115818+DEVASHISH7777@users.noreply.github.com>
2020-12-31 18:25:51 +0100

    ajout des collisions

--- backend/python/controleur.py
+++ backend/python/controleur.py
@@ -7,22 +7,32 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.vie = 1
-        self.vitesse = 10
+        self.vitesse = 5
         self.image = pygame.image.load('img/saitama1.png')
         self.princess = pygame.image.load('img/princesse.png')
         self.rect = self.image.get_rect()
-        self.rect.x = 400
-        self.rect.y = 580
+        self.rect.x = 425
+        self.rect.y = 950
 
 
     def move_right(self):
+    if not self.game.check_collision(self, self.game.all_obstacle):
         self.rect.x += self.vitesse
+        print("tu es mort")
+
 
     def move_left(self):
-        self.rect.x -= self.vitesse
+        if not self.game.check_collision(self, self.game.all_obstacle):
+            self.rect.x -= self.vitesse
+            print("tu es mort")
 
     def move_up(self):
-        self.rect.y -= self.vitesse
+        if not self.game.check_collision(self, self.game.all_obstacle):
+            self.rect.y -= self.vitesse
+            print("tu es mort")
+
 
     def move_down(self):
-        self.rect.y += self.vitesse
+        if not self.game.check_collision(self, self.game.all_obstacle):
+            self.rect.y += self.vitesse
+            print("tu es mort")
