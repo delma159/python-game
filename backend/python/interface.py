f6adc3a0bc960183def1666df8a12640a3070331
DEVASHISH7777 <63115818+DEVASHISH7777@users.noreply.github.com>
2020-12-31 17:13:45 +0100

    Update interface.py

--- backend/python/interface.py
+++ backend/python/interface.py
@@ -1,60 +1,200 @@
-from tkinter import *
-from map import *
-from items import Items
+import pygame
+from game import Game
+
+pygame.init()
+
+"""génération de la fenetre"""
+
+pygame.display.set_caption("Princess Rescue")
+screen = pygame.display.set_mode((1024, 720))
+background = pygame.image.load('img/BK.png')
+
+"""chargement du jeu"""
+
+game = Game()
+
+running = True
+
+"""boucle running"""
+while running:
+
+    screen.blit(background, (0, 0))
+
+    """chargement personnages"""
+    screen.blit(game.player.image, game.player.rect)
+    screen.blit(game.player.princess, (0, 0))
+
+    pygame.display.flip()
+
+    """condition de fin"""
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            print("fin de jeu")
+
+        elif event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_RIGHT:
+                game.player.move_right()
+
+            if event.key == pygame.K_LEFT:
+                game.player.move_left()
+
+            if event.key == pygame.K_DOWN:
+                game.player.move_down()
+
+            if event.key == pygame.K_UP:
+                game.player.move_up()
 
 
 
-class MyApp:
-
-    def test(self):
-        Map = MapPrincess(0, 0)
-        item = Items()
-        game = Controleur(liste_map=Map.generate_map())
-        print('test')
-        game.affichage_map()
-        #print(game.liste_map)
-
-    def __init__(self):
-        self.window = Tk()
-        self.window.title("Princess Rescue")
-        self.window.geometry("1080x720")
-        self.window.minsize(480, 360)
-        self.window.iconbitmap("")
-        self.window.config(background='#373C3C')
-
-        # initialization des composants
-        self.frame = Frame(self.window, bg='#41B77F')
-
-        # creation des composants
-        self.create_widgets()
-
-        # empaquetage
-        self.frame.pack(expand=YES)
-
-    def create_widgets(self):
-        self.create_title()
-        self.create_subtitle()
-        self.create_button()
-
-    def create_title(self):
-        label_title = Label(self.frame, text="Princess Rescue", font=("Courrier", 40), bg='#537272',fg='white')
-        label_title.pack()
-
-    def create_subtitle(self):
-        label_subtitle = Label(self.frame, text="Commencez la partie", font=("Courrier", 25), bg='#41B77F', fg='white')
-        label_subtitle.pack()
-
-    def create_button(self):
-        #Map = MapPrincess(0,0)
-        #game = Controleur(liste_map=Map.generate_map())
-        #item = Items()
-        label_button = Button(self.frame, text="start", font=('Courrier', 25), bg='White', command=self.test)
-        label_button.pack()
 
 
-#command=game.afficher_map
 
 
-# afficher
-app = MyApp()
-app.window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
