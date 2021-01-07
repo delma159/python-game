23288f8025dfefc1c125d015aed73e90ae269122
DEVASHISH7777 <63115818+DEVASHISH7777@users.noreply.github.com>
2021-01-07 17:00:25 +0100

    programmation par contrat + ajout function , raise
    ajout des pre , post raise,
    + plus raise dans innit
    + function damage

--- backend/python/controleur.py
+++ backend/python/controleur.py
@@ -3,6 +3,8 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
+    """Class represantant le joueur et la princess
+    """
 
     def __init__(self):
         super().__init__()
@@ -13,26 +15,62 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 425
         self.rect.y = 950
+        
+        if self.vie == 0:
+            raise ValueError
 
+        if self.vitesse == 0:
+            raise ValueError
 
-       def move_right(self):
-        if not self.game.check_collision(self, self.game.all_obstacle):
-            self.rect.x += self.vitesse
-            print("tu es mort")
+        """Creation de function permet le deplacement a droit , verification de collison pour restreindre ces deplacements
+        PRE : -
+        POST : deplacement vers la droit + verification collision
+        """
+            
 
+   def move_right(self):
+    if not self.game.check_collision(self, self.game.all_obstacle):
+        self.rect.x += self.vitesse
+        
+        
+    """Creation de function permet le deplacement a gauche , verification de collison pour restreindre ces deplacements
+    PRE : -
+    POST : deplacement vers la gauche + verification collision
+        """
 
     def move_left(self):
         if not self.game.check_collision(self, self.game.all_obstacle):
             self.rect.x -= self.vitesse
-            print("tu es mort")
-
+    
+    """Creation de function permet le deplacement en haut , verification de collison pour restreindre ces deplacements
+    PRE : -
+    POST : deplacement vers le haut + verification collision
+        """
+    
     def move_up(self):
         if not self.game.check_collision(self, self.game.all_obstacle):
             self.rect.y -= self.vitesse
-            print("tu es mort")
-
+            
+            
+    """Creation de function permet le deplacement en bas , verification de collison pour restreindre ces deplacements
+    PRE : -
+    POST : deplacement vers le bas + verification collision
+        """
 
     def move_down(self):
         if not self.game.check_collision(self, self.game.all_obstacle):
             self.rect.y += self.vitesse
-            print("tu es mort")
+
+            
+            
+     """Creation function permetant la gestion des degats et point de vie du joueur
+    PRE : @param{degat} : valeur(s) degat subit par le joueur
+    POST : difference entre la vie et degat
+    """
+
+    def damage(self, degat):
+        if self.vie - degat > degat:
+            self.vie -= degat
+
+        else:
+            self.game.game_over()
