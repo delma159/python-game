e4fd7ee3edad6404c356ded04b0dc54cc353acc6
delma159 <59198120+delma159@users.noreply.github.com>
2021-03-23 09:49:18 +0100

    Update test.py
    creation de test de liste + test user + test déplacement

--- backend/python/test/test.py
+++ backend/python/test/test.py
@@ -6,18 +6,24 @@ import unittest
 class test(unittest.Testcase):
     def setUp(self):
         self.Map = MapPrincess(0, 0)
+
     def test_map_largeur_is_ok(self):
-        
-        self.assertGreaterEqual(MapPrincess.largeur, 5)
-        self.assertLessEqual(MapPrincess.largeur, 10)
+        self.assertGreaterEqual(self.MapPrincess.largeur, 5)
+        self.assertLessEqual(self.MapPrincess.largeur, 10)
 
     def test_map_longeur_is_ok(self):
-        self.assertGreaterEqual(MapPrincess.longeur, 5)
-        self.assertLessEqual(MapPrincess.longeur, 10)
+        self.assertGreaterEqual(self.MapPrincess.longeur, 5)
+        self.assertLessEqual(self.MapPrincess.longeur, 10)
 
-    def test_move(self):
-        Map = MapPrincess(0, 0)
-        self.assert
+    def test_deplacement(self):
+        self.assertTrue(self.deplacement)
+
+    def test_user(self):
+        self.assertGreater(self.User.point, 0)
+
+    def test_liste_map_vide(self):
+        listeMap = []
+        self.assertFalse(self.MapPrincess.listeMap)
 
 if __name__ == '__main__':
     unittest.main()
