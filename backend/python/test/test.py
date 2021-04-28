75042eae6168a44625e594f0bd3822d73dddef91
delma159 <59198120+delma159@users.noreply.github.com>
2021-04-28 14:25:34 +0200

    tests fonctionnels
    correction des bugs

--- backend/python/test/test.py
+++ backend/python/test/test.py
@@ -1,31 +1,35 @@
 import sys
 sys.path.append("..")
-from src.map import MapPrincess
+from map import MapPrincess
 from user import User
 import unittest
 
-class test(unittest.Testcase):
+class test(unittest.TestCase):
+
     def setUp(self):
-        self.Map = MapPrincess(0, 0)
+        self.Map = MapPrincess(5, 5)
+        self.User = User("adel", 3)
+
 
     def test_map_largeur_is_ok(self):
-        self.assertGreaterEqual(self.MapPrincess.largeur, 5)
-        self.assertLessEqual(self.MapPrincess.largeur, 10)
+        self.assertGreaterEqual(self.Map.largeur, 5)
+        self.assertLessEqual(self.Map.largeur, 10)
 
     def test_map_longeur_is_ok(self):
-        self.assertGreaterEqual(self.MapPrincess.longeur, 5)
-        self.assertLessEqual(self.MapPrincess.longeur, 10)
-
+        self.assertGreaterEqual(self.Map.longeur, 5)
+        self.assertLessEqual(self.Map.longeur, 10)
+    """
     def test_deplacement(self):
-        self.assertTrue(self.deplacement)
+        self.assertTrue(self.)"""
 
     def test_user(self):
         self.assertGreater(self.User.point, 0)
 
     def test_liste_map_vide(self):
-        listeMap = []
-        self.assertFalse(self.MapPrincess.listeMap)
+        self.assertFalse(self.Map.listeMap)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
-    
